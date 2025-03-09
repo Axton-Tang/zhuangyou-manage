53e8a8ff219ae7731062accc7303389687d37fb0
Axton-Tang <63949855+Axton-Tang@users.noreply.github.com>
2025-03-09 15:21:14 +0800

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Welcome to Zhuangyou Manage</h1>
+      <p>Start building your application here!</p>
+    </main>
+  );
+}
